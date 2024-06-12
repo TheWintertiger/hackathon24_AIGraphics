ddc77107caff7000f8deaf50611bbe363e758c78
Mitja B <mitja.basle@gmail.com>
2024-06-12 10:04:14 +0200

    Adding some project handling and workspace files.

---
+++ #1 ActivateVENV.ps1
@@ -0,0 +1,9 @@
+# Write-Output "This scripts Activates Python virtual environment created in script #0"
+# Write-Output "Run it on on start of working"
+Clear-Host
+Write-Output "****************************************"
+Write-Output ""
+Write-Output "Activating Python virtual environment..."
+#Set-Location -Path .\Software
+#Set-Location DjangoPythonListApp
+.\.venv\Scripts\Activate.ps1
---
+++ README.md
@@ -0,0 +1,9 @@
+# Best way is to create VENV to install all dependecies locally not to path Python.
+  1. Run #0 CreatePythonVENV.ps1 script
+
+
+# Executing code
+  1. Have the .venv environment activated (you can run #1 ActivateVENV.ps1 script for that or select it/configure it in the project)
+  2. Execute code from "cppns_tasking.ipynb"
+
+  
